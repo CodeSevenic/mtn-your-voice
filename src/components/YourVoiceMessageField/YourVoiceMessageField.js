4c485673b269497144ae861cd62e174dfd651ef9
CodeSevenic <sevenicbookings@gmail.com>
2022-10-04 16:30:29 +0200

    first commit

---
+++ src/components/YourVoiceMessageField/YourVoiceMessageField.js
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import './YourVoiceMessageField.css';
+
+const YourVoiceMessageField = () => {
+  return (
+    <div className="msg-container">
+      <div className="input-data">
+        <input type="text" placeholder="Add your voice" />
+        <label htmlFor=""></label>
+      </div>
+    </div>
+  );
+};
+
+export default YourVoiceMessageField;
