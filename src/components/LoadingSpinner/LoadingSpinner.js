f9943bdad3453d06118e944ed570a228b16d5cbe
CodeSevenic <sevenicbookings@gmail.com>
2022-10-05 05:25:10 +0200

    Code update

---
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -0,0 +1,23 @@
+﻿import React from 'react';
+import './LoadingSpinner.css';
+
+const LoadingSpinner = () => {
+  return (
+    <div class="lds-spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
--- src/components/YourVoiceMessageField/YourVoiceMessageField.js
+++ src/components/YourVoiceMessageField/YourVoiceMessageField.js
@@ -1,17 +1,39 @@
-﻿import React from 'react';
+﻿import React, { useState } from 'react';
+import { AttentionIcon, CloseIcon } from '../svg/SvgIcons';
 import './YourVoiceMessageField.css';
 
 const YourVoiceMessageField = () => {
+  const [charCount, setCharCount] = useState(30);
+  const [charLeft] = useState(30);
+
+  const handleCharCount = (e) => {
+    let input = e.target.value;
+    setCharCount(charLeft - input.length);
+  };
+
   return (
     <div className="msg-container">
+      <CloseIcon />
       <div className="input-data">
-        <textarea type="text" placeholder="Add your voice" />
-        <p style={{ color: '#249847' }} className="character-count">
-          characters
+        <textarea
+          onChange={(e) => handleCharCount(e)}
+          type="text"
+          rows={`${charCount < 0 ? '2' : '1'}`}
+          placeholder="Add your voice"
+          style={{ color: `${charCount >= 0 ? '#000000' : '#FF0000'}` }}
+        />
+        <p
+          style={{ color: `${charCount >= 0 ? '#249847' : '#FF0000'}` }}
+          className="character-count"
+        >
+          {charCount} characters
         </p>
         <button>
           <a href="">Submit</a>
         </button>
+        <p className="warning-msg">
+          <AttentionIcon /> Your message will be public
+        </p>
       </div>
     </div>
   );
